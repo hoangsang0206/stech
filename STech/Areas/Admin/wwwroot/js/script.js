32b8f2a90f65b0884171ed47711d487ee6092031
Sang <lehoangsang02062003@gmail.com>
2024-08-05 20:02:14 +0700

    Update admin layout

---
+++ STech/Areas/Admin/wwwroot/js/script.js
@@ -0,0 +1,14 @@
+﻿$('.sidebar-arrow').click(function () {
+    $(this).closest('li').find('.sidebar-expandation').toggleClass('show');
+    $(this).toggleClass('active');
+})
+
+$('.show-user-action').click(function () {
+    $('.user-action').toggleClass('show');
+    $(this).toggleClass('active');
+})
+
+$('.show-notification').click(function () {
+    $('.notification-content').toggleClass('show');
+    $(this).toggleClass('active');
+})
